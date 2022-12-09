08c0a7386443e9e79de86d0223dbf91dbdbe78c5
John Oerter <john@johnoerter.me>
2022-12-09 11:07:26 -0500

    fix ts error

--- app/routes/notes/new.tsx
+++ app/routes/notes/new.tsx
@@ -109,7 +109,7 @@ export default function NewNotePage() {
         <button
           type="submit"
           className="rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-          disabled={transition.submission}
+          disabled={transition.submission != null}
         >
           {transition.submission ? "Saving note..." : "Save"}
         </button>
